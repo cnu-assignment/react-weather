0fb63b69c01f3dfd64221b000cb0cde1852c947a
jihyun <jhhhj1002@hanmail.net>
2019-05-24 19:33:51 +0900

    blah

---
+++ src/Component/Weather/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+//const API_CITIES ='http://api.openweathermap.org/data/2.5/weather?q={cityName}&APPID={apiKey}';
+
+const API_CITIES = 'http://localhost:27017/weather-crawlers,cities'; //weather 할때 썼던 주소
+//const API_CITIES = '';
+
+class Weather extends React.Component {
+    state = {
+        cities : null
+    };
+
+    async componentDidMount(){
+
+        const city_list = await fetch(API_CITIES)
+            .then(res => res.json())
+            .then(data => data);
+
+        console.log(city_list);
+
+        this.setState({
+            cities: city_list
+        });
+//        console.log('CDM!! ');
+//
+//        console.log(this.state.foo);
+//
+//        this.setState({
+//            foo: 'HELLO WORLD'
+//        });
+//
+//        console.log(this.state.foo);
+    }
+
+    render(){
+        const {cities} = this.state;
+
+        if(!cities){
+            return (<div>Loading...</div>);
+        }
+        return (
+            <div>
+                <ul>
+                    {cities.map(item =>{
+                        return <li key= {item}>{item}</li>;
+                     })}
+                </ul>
+            </div>
+        );
+
+    }
+}
+
+export default Weather;
---
+++ src/RouterApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Header from './Header';
+import Weather from'./Component/Weather';
+
+
+const About = () => <div>About</div>
+
+
+const RouterApp = () => {
+    const city = 'Daejeon';
+    return(
+
+        <div className = "App">
+           <Header city = {city}/>
+           <Route exact path="/" component={Weather} />
+           <Route path="/about" component={About} />
+
+        </div>
+    );
+};
+
+export default RouterApp;
+
